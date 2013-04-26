98840c0c48e766b7a1643b291c3e712f6ab6cd92
root <root@SNDA-172-17-15-82.(none)>
2013-04-26 23:49:44 +0800

    change conf for getprice.py

--- conf/conf.py
+++ conf/conf.py
@@ -5,14 +5,14 @@ def init(conf):
     data = {
 	"ips":["118.244.224.3"],
         "master_ip":"118.244.224.211",
-	"data_file":"/root/data/urls.txt",
+	"data_file":"/root/data/pricequery.txt",
 	"split_tmp":"/root/split_tmp",
 	"master_result":"/root/result",
 	"client_result":"/root/result",
         "client_union":"/root/union",
         "client_union_name":"result.txt",
         "task_num":150,
-        "process":"../process/test.py",
+        "process":"../process/getprice.py",
         "split_prefix":"urls",
         "merge":"../result_merge/union.py",
         "client_id":"/root/clientid.txt"
